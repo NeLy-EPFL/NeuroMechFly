1a2e8157b684ebcf64a97135bc1f05ee3c57467a
Shravan Tata Ramalingasetty <tatarama@>
2021-04-12 03:05:01 +0200

    [OPT] Added alternative for lava check

--- scripts/Optimization/drosophila_simulation_opt.py
+++ scripts/Optimization/drosophila_simulation_opt.py
@@ -313,8 +313,11 @@ class DrosophilaSimulation(BulletSimulation):
         #return (self.distance_y < (((self.TIME)/self.RUN_TIME)*2)-0.25)
         ball_rot = np.array(self.ball_rotations())
         dist_traveled = -ball_rot[0]*self.ball_radius # Distance in mm
+        # ball_rot_speed = -ball_rot[0]/self.time
         #print(dist_traveled)
         moving_limit = (((self.time)/self.run_time)*3*np.pi)-0.5*np.pi
+        # moving_limit = (((self.time)/self.run_time)*1.0) - 0.25
+        # return ball_rot_speed < moving_limit
         return dist_traveled < moving_limit
         #return (dist_traveled < (((self.TIME)/self.RUN_TIME)*2)-0.25)
 
