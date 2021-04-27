f16fbf9d2a0ae97a813523ab7a91d77867cd2715
Shravan Tata Ramalingasetty <tatarama@>
2021-04-27 11:04:43 +0200

    [SDF] Added licenses to modules
    TODO : We need to remove this package before release

--- NeuroMechFly/sdf/bullet_load_sdf.py
+++ NeuroMechFly/sdf/bullet_load_sdf.py
@@ -1,4 +1,24 @@
-"""SDF"""
+"""
+-----------------------------------------------------------------------
+Copyright 2018-2020 Jonathan Arreguit, Shravan Tata Ramalingasetty
+Copyright 2018 BioRobotics Laboratory, École polytechnique fédérale de Lausanne
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-----------------------------------------------------------------------
+
+SDF
+
+"""
 
 import os
 import numpy as np
@@ -178,7 +198,7 @@ def load_sdf(
     base_link_index = None
     parenting = {joint.child: joint.parent for joint in sdf.joints}
     for link in sdf.links:
-        
+
         # Number of visuals/collisions in link
         n_links = max(1, len(link.visuals), len(link.collisions))
 
