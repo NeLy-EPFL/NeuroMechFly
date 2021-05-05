1e9e061aeb02c18d535d333456dbac159d5f4aa8
Shravan Tata Ramalingasetty <shravan.tataramalingasetty@epfl.ch>
2021-05-05 23:34:53 +0200

    [SIM] Added option to set using concave meshes or not
    By default is False unless the behavior is set to grooming

--- NeuroMechFly/simulation/bullet_simulation.py
+++ NeuroMechFly/simulation/bullet_simulation.py
@@ -58,6 +58,10 @@ class BulletSimulation(metaclass=abc.ABCMeta):
         self.rotate_camera = kwargs.get('rot_cam', False)
         self.behavior = kwargs.get('behavior', None)
         self.ground = kwargs.get('ground', 'ball')
+        self.enable_concave_mesh = kwargs.get(
+            'enable_concave_mesh',
+            True if self.behavior == 'grooming' else False
+        )
         self.self_collisions = kwargs.get('self_collisions', [])
         self.draw_collisions = kwargs.get('draw_collisions', False)
 
@@ -200,7 +204,9 @@ class BulletSimulation(metaclass=abc.ABCMeta):
 
         #: Add the animal model
         if '.sdf' in self.model:
-            self.animal, self.link_id, self.joint_id = load_sdf(self.model)
+            self.animal, self.link_id, self.joint_id = load_sdf(
+                self.model, force_concave=self.enable_concave_mesh
+            )
         elif '.urdf' in self.model:
             self.animal = p.loadURDF(self.model)
         p.resetBasePositionAndOrientation(
