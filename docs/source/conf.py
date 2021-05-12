e5ef3a04c1acfe056afd8698c5538a3343267cae
Shravan Tata Ramalingasetty <shravan.tataramalingasetty@epfl.ch>
2021-05-12 15:18:07 +0200

    [DOCS] Added new source files

---
+++ docs/source/conf.py
@@ -0,0 +1,162 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+# -- General configuration -----------------------------------------------------
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax',
+              'sphinx.ext.viewcode', 'sphinx.ext.napoleon',
+              'sphinx.ext.intersphinx']
+
+autodoc_default_flags = ['show-inheritance']
+autoclass_content = "both"
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# The master toctree document.
+master_doc = 'index'
+
+
+# -- HTML ------------------------------------------------------------------
+
+import sphinx_rtd_theme
+from sphinx.builders.html import StandaloneHTMLBuilder
+
+# -- Project information -----------------------------------------------------
+
+project = 'neuromechfly'
+copyright = '2021, nely'
+author = 'nely'
+
+# The full version, including alpha/beta/rc tags
+_d = {}
+fname = os.path.join(os.pardir, os.pardir, 'NeuroMechFly', '__init__.py')
+exec(compile(open(fname).read(), fname, 'exec'), _d)
+version = release = _d['__version__']
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.autodoc',]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+StandaloneHTMLBuilder.supported_image_types = [
+    'image/gif', 'image/png', 'image/jpeg'
+]
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'NeuroMechFlydoc'
+
+# -- Options for LaTeX output --------------------------------------------------
+
+latex_elements = {
+# The paper size ('letterpaper' or 'a4paper').
+#'papersize': 'letterpaper',
+
+# The font size ('10pt', '11pt' or '12pt').
+#'pointsize': '10pt',
+
+# Additional stuff for the LaTeX preamble.
+#'preamble': '',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, documentclass [howto/manual]).
+latex_documents = [
+  ('index', 'NeuroMechFly.tex', u'NeuroMechFly Documentation',
+   u'NeuroMechFly developers', 'manual'),
+]
+
+# The name of an image file (relative to this directory) to place at the top of
+# the title page.
+#latex_logo = None
+
+# For "manual" documents, if this is true, then toplevel headings are parts,
+# not chapters.
+#latex_use_parts = False
+
+# If true, show page references after internal links.
+#latex_show_pagerefs = False
+
+# If true, show URL addresses after external links.
+#latex_show_urls = False
+
+# Documents to append as an appendix to all manuals.
+#latex_appendices = []
+
+# If false, no module index is generated.
+#latex_domain_indices = True
+
+
+# -- Options for manual page output --------------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    ('index', 'neuromechfly', u'NeuroMechFly Documentation',
+     [u'NeuroMechFly developers'], 1)
+]
+
+# If true, show URL addresses after external links.
+#man_show_urls = False
+
+
+# -- Options for Texinfo output ------------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+  ('index', 'NeuroMechFly', u'NeuroMechFly Documentation',
+   u'NeuroMechFly developers', 'NeuroMechFly', 'One line description of project.',
+   'Miscellaneous'),
+]
+
+# Documents to append as an appendix to all manuals.
+#texinfo_appendices = []
+
+# If false, no module index is generated.
+#texinfo_domain_indices = True
+
+# How to display URL addresses: 'footnote', 'no', or 'inline'.
+#texinfo_show_urls = 'footnote'
---
+++ docs/source/reference/utils.rst
@@ -0,0 +1,20 @@
+Subpackage utils
+================
+
+options
+-------
+
+.. automodule:: NeuroMechFly.utils.options
+   :members:
+
+plotting
+--------
+
+.. automodule:: NeuroMechFly.utils.plotting
+   :members:
+
+sensitivity_analysis
+--------------------
+
+.. automodule:: NeuroMechFly.utils.sensitivity_analysis
+   :members:
