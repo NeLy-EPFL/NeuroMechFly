1cbc483a01b716afb8fec6b39b3a6e851a04deb4
Shravan Tata Ramalingasetty <shravan.tataramalingasetty@epfl.ch>
2021-05-14 18:41:18 +0200

    [DOCS] Debug pip install issue

--- docs/buildsite.sh
+++ docs/buildsite.sh
@@ -10,8 +10,16 @@ export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
 ###################
 # INSTALL PACKAGE #
 ###################
+export PIP_CACHE_DIR=~/.local/pip-cache
+mkdir -p $PIP_CACHE_DIR
+python3 -m pip cache dir
+ls -l $PIP_CACHE_DIR
 python3 -m venv nmf
 source nmf/bin/activate
+which pip
+pip -V
+python3 -V
+python -m pip cache dir
 python -m pip install . --user
 
 ##############
