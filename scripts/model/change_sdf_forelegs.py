ce248ba0fea87a91c4814e18f44369bb088706be
GizemOzdil <pembe.ozdil@epfl.ch>
2021-11-10 19:24:59 +0100

    [SCRIPTS] formatting; docstring; file path check

--- scripts/model/change_sdf_forelegs.py
+++ scripts/model/change_sdf_forelegs.py
@@ -1,4 +1,7 @@
-""" Replace Drosophila SDF Template """
+""" This scripts takes the default NeuroMechFly template SDF and replaces the links with cylinders.
+
+To make further changes, simply replace the link name and provide the size and name of the new geometric shape.
+"""
 
 from pathlib import Path
 
@@ -12,7 +15,7 @@ SCRIPT_PATH = Path(__file__).parent.absolute()
 DATA_PATH = SCRIPT_PATH.joinpath("..", "..", "data")
 CONFIG_PATH = DATA_PATH.joinpath("config")
 POSE_CONFIG_PATH = CONFIG_PATH.joinpath("pose")
-SDF_MODEL_PATH =  DATA_PATH.joinpath("design", "sdf")
+SDF_MODEL_PATH = DATA_PATH.joinpath("design", "sdf")
 
 units = SimulationUnitScaling(
     meters=1e0,
@@ -32,18 +35,18 @@ def generate_new_sdf(model, radius, new_model_name, **kwargs):
         Radius of the cylinders that will replace forelegs
     new_model_name : str
         Name of the new SDF file.
-    """    
+    """
     link_index = utils.link_name_to_index(model)
     joint_index = utils.joint_name_to_index(model)
 
     leg_segments = {
-        'Coxa': 0.425, 
-        'Femur': 0.706, 
-        'Tibia': 0.518, 
-        'Tarsus1': 0.663, 
-        'Tarsus4': 0, 
-        'Tarsus3': 0, 
-        'Tarsus2': 0, 
+        'Coxa': 0.425,
+        'Femur': 0.706,
+        'Tibia': 0.518,
+        'Tarsus1': 0.663,
+        'Tarsus4': 0,
+        'Tarsus3': 0,
+        'Tarsus2': 0,
         'Tarsus5': 0
     }
 
@@ -55,68 +58,69 @@ def generate_new_sdf(model, radius, new_model_name, **kwargs):
             link_name = f'{side}F{segment_name}'
             if length == 0:
                 model.links[link_index[link_name]].visuals = [
-                Link.empty(
-                    name = link_name,
-                    pose = [0, 0, 0, 0, 0, 0],
-                    units=units
-                )
+                    Link.empty(
+                        name=link_name,
+                        pose=[0, 0, 0, 0, 0, 0],
+                        units=units
+                    )
                 ]
                 model.links[link_index[link_name]].collisions = [
-                Link.empty(
-                    name = link_name,
-                    pose = [0, 0, 0, 0, 0, 0],
-                    units=units
-                )
+                    Link.empty(
+                        name=link_name,
+                        pose=[0, 0, 0, 0, 0, 0],
+                        units=units
+                    )
                 ]
             else:
                 model.links[link_index[link_name]].visuals = [
                     Visual.cylinder(
-                        name = link_name,
-                        radius = radius,
-                        length = length,
-                        pose = [0, 0, -length/2, 0, 0, 0],
-                        units = units
+                        name=link_name,
+                        radius=radius,
+                        length=length,
+                        pose=[0, 0, -length / 2, 0, 0, 0],
+                        units=units
                     )
                 ]
 
                 model.links[link_index[link_name]].collisions = [
                     Collision.cylinder(
-                        name = link_name,
-                        radius = radius,
-                        length = length,
-                        pose = [0, 0, -length/2, 0, 0, 0],
-                        units = units
+                        name=link_name,
+                        radius=radius,
+                        length=length,
+                        pose=[0, 0, -length / 2, 0, 0, 0],
+                        units=units
                     )
                 ]
 
             model.links[link_index[f'{side}Antenna']].visuals = [
                 Visual.cylinder(
-                    name = f'{side}Antenna',
-                    radius = radius_antenna,
-                    length = length_antenna,
-                    pose = [0, 0, -length_antenna/2, 0, 0, 0],
-                    units = units
+                    name=f'{side}Antenna',
+                    radius=radius_antenna,
+                    length=length_antenna,
+                    pose=[0, 0, -length_antenna / 2, 0, 0, 0],
+                    units=units
                 )
             ]
 
             model.links[link_index[f'{side}Antenna']].collisions = [
                 Collision.cylinder(
-                    name = f'{side}Antenna',
-                    radius = radius_antenna,
-                    length = length_antenna,
-                    pose = [0, 0, -length_antenna/2, 0, 0, 0],
-                    units = units
+                    name=f'{side}Antenna',
+                    radius=radius_antenna,
+                    length=length_antenna,
+                    pose=[0, 0, -length_antenna / 2, 0, 0, 0],
+                    units=units
                 )
             ]
 
     model.write(SDF_MODEL_PATH.joinpath(new_model_name))
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     """ Main. """
-    
+
     # Read the sdf model from template
-    model = ModelSDF.read(SDF_MODEL_PATH.joinpath("neuromechfly_noLimits.sdf"))[0]
+    model = ModelSDF.read(SDF_MODEL_PATH.joinpath(
+        "neuromechfly_noLimits.sdf"))[0]
 
     # New model name
     new_model_name = "neuromechfly_frontleg_cylinder.sdf"
